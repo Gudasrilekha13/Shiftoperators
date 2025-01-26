4743e601b37dbd188686b5ad5d8327bdebcb62b7
Gudasrilekha13 <gudasrilekha@gmail.com>
2025-01-26 17:35:39 +0530

    Add files via upload

---
+++ Shiftoperators.java
@@ -0,0 +1,9 @@
+public class Shiftoperators {
+    public static void main(String[]args){
+        int num=12;
+        int rightshift=num>>1;
+        System.out.println("num:"+rightshift);
+   }
+}
+    
+
